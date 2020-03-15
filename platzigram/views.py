ff1cf26e78308131acdd913878d67756838b326d
Antonio <antonio241710@gmail.com>
2020-03-15 11:14:59 +0100

    Solución al reto - Pasando argumentos en la URL

--- platzigram/views.py
+++ platzigram/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponse
 
 # Utilities
 from datetime import datetime
+import json
 
 
 def hello_world(request):
@@ -13,7 +14,25 @@ def hello_world(request):
     return HttpResponse('Oh, hi! Current server time is {now}'.format(now=now))
 
 
-def hi(request):
-    """Hi."""
-    numbers = request.GET['numbers']
-    return HttpResponse('Numbers: {numbers}'.format(numbers=numbers))
+def sort_integers(request):
+    """Return a JSON response with sorted integers."""
+    numbers = [int(i) for i in request.GET['numbers'].split(',')]
+    sorted_ints = sorted(numbers)
+    data = {
+        'status': 'ok',
+        'numbers': sorted_ints,
+        'message': 'Integers sorted succesfully.'
+    }
+    return HttpResponse(
+        json.dumps(data, indent=4),
+        content_type='application/json'
+    )
+
+
+def say_hi(request, name, age):
+    """"Return a greeting."""
+    if age < 12:
+        message = 'Sorry {} you not allowed here'.format(name)
+    else:
+        message = 'Hello, {}! Welcome to Platzigram'.format(name)
+    return HttpResponse(message)
