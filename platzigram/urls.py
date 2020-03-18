8e2e878a7b65342c5e09ca58f29b1ee39e6d49a5
Antonio <antonio241710@gmail.com>
2020-03-18 14:50:02 +0100

    Extendiendo el modelo de usuario

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -1,11 +1,15 @@
 """Platzigram URLs module."""
 
+# Django
+from django.contrib import admin
 from django.urls import path
 
 from platzigram import views as local_views
 from posts import views as posts_views
 
 urlpatterns = [
+
+    path('admin/', admin.site.urls),
 
     path('hello-world/', local_views.hello_world),
     path('sorted/', local_views.sort_integers),
